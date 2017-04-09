200b88382481d64413c39dbb757af795e12847d6
matkma <kmakmateusz@gmail.com>
2017-04-09 17:44:46 +0200

    init

--- CoreReactIntegrationDemo/Controllers/HomeController.cs
+++ CoreReactIntegrationDemo/Controllers/HomeController.cs
@@ -2,34 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreReactIntegrationDemo.Models;
 using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CoreReactIntegrationDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> comments;
+
+        static HomeController()
+        {
+            comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Negro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walker",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            return Json(comments);
         }
 
-        public IActionResult Contact()
+        [Route("comments/new")]
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            comment.Id = comments.Count + 1;
+            comments.Add(comment);
+            return Content("Success");
         }
     }
 }
